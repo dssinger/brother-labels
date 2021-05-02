098cd8148c9b6581c3e5397c6777ec2c38391a9f
David Singer <david@dsinger.com>
2021-05-02 13:26:48 -0700

    Make the printer a parameter

--- labels.py
+++ labels.py
@@ -11,6 +11,7 @@ parser.add_argument('--height', type=int, help="height in pixels of loaded tape"
 parser.add_argument('--font', type=str, help="name of truetype font", default="Avenir Next Condensed")
 parser.add_argument('--size', type=int, default=30)
 parser.add_argument('--variation', type=str, nargs='+', default='')
+parser.add_argument('--printer', type=str, default='Brother_PT_2730')
 args = parser.parse_args()
 
 if len(args.text) == 0:
@@ -34,4 +35,4 @@ draw.text((0, 0), text, font=font, fill=(0, 0, 0))
 image.save('out.png')
 width = size[0]
 print(size, file=sys.stderr)
-print(f'lpr -P Brother_PT_2730_2 -o media=12mm -o PageSize=Custom.28x{width} out.png')
+print(f'lpr -P "{args.printer}" -o media=12mm -o PageSize=Custom.28x{width} out.png')
